7cd7cceafc0d28bb9bf831dedce953bef5b1aaa7
Muhammad Helmi Hibatullah <helmihibatullah52@gmail.com>
2024-11-12 20:59:01 +0700

    Time: 12 ms (17.76%), Space: 45.4 MB (17.98%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,24 +1,23 @@
 class Solution {
     public String reverseVowels(String s) {
         List<String> vowels = Arrays.asList("a", "e", "i", "o", "u", "A", "I", "U", "E", "O");
-        List<String> vowelsInS = new ArrayList<String>();
-        List<Integer> vowelsIdx = new ArrayList<Integer>();
-        for (int i = 0; i < s.length(); i++) {
-            if (vowels.contains(String.valueOf(s.charAt(i)))) {
-                vowelsInS.add(String.valueOf(s.charAt(i)));
-                vowelsIdx.add(i);
+        StringBuilder res = new StringBuilder(s);
+        int i = 0, j = s.length() - 1;
+        while (i < j && i < s.length() && j > 0) {
+            while (i < s.length() && !vowels.contains(String.valueOf(s.charAt(i)))) {
+                i++;
+            }
+            while (j >= 0 && !vowels.contains(String.valueOf(s.charAt(j)))) {
+                j--;
+            }
+            if (i < j && i < s.length() && j > 0) {
+                res.setCharAt(i, s.charAt(j));
+                res.setCharAt(j, s.charAt(i));
+                i++;
+                j--;
             }
         }
 
-        List<String> reversed = new ArrayList<String>();
-        for (int i = vowelsInS.size() - 1; i >= 0; i--) {
-            reversed.add(vowelsInS.get(i));
-        }
-
-        StringBuilder res = new StringBuilder(s);
-        for (int i = 0; i < vowelsInS.size(); i++) {
-            res.setCharAt(vowelsIdx.get(i), reversed.get(i).charAt(0));
-        }
         return res.toString();
     }
 }
