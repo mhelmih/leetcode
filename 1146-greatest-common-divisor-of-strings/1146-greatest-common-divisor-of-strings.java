0734b0d8476d9c5d1d9b193da538f7c6d239f8b2
Muhammad Helmi Hibatullah <helmihibatullah52@gmail.com>
2024-11-12 15:20:30 +0700

    Time: 1 ms (66.48%), Space: 42.3 MB (35.04%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -1,21 +1,19 @@
 class Solution {
-    public boolean isDivisible(String str1, String str2, int i) {
-        if (str1.length() % i > 0 && str2.length() % i > 0) {
-            return false;
+    public int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
         }
-        String prefix = str1.substring(0, i);
-        return str1.replace(prefix, "").isEmpty() && str2.replace(prefix, "").isEmpty();
+        return gcd(b, a % b);
     }
 
     public String gcdOfStrings(String str1, String str2) {
         String res = "";
-        for (int i = Math.min(str1.length(), str2.length()); i > 0; i--) {
-            if (isDivisible(str1, str2, i)) {
-                res = str1.substring(0, i);
-                break;
-            }
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return res;
         }
 
+        int gcd = gcd(str1.length(), str2.length());
+        res = str1.substring(0, gcd);
         return res;
     }
 }
