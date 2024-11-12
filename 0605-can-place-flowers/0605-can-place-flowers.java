5e8fac156ca2ed1256eb221c7bf711ca28136b7a
Muhammad Helmi Hibatullah <helmihibatullah52@gmail.com>
2024-11-12 18:10:01 +0700

    Time: 1 ms (96.75%), Space: 45.2 MB (93.58%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,63 +1,22 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int i = 0;
         int remainder = n;
-        while (i < flowerbed.length && remainder > 0) {
-            if (flowerbed.length == 1) {
-                if (flowerbed[i] == 0) {
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                boolean emptyLeft = (i == 0) || (flowerbed[i - 1] == 0);
+                boolean emptyRight = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
+                
+                if (emptyLeft && emptyRight) {
                     flowerbed[i] = 1;
                     remainder -= 1;
-                } else {
-                    i += 1;
-                }
-            }
-
-            if (i == 0) {
-                if (flowerbed[i] == 0) {
-                    if (flowerbed[i + 1] == 0) {
-                        flowerbed[i] = 1;
-                        remainder -= 1;
-                        i += 2;
-                    } else {
-                        i += 3;
+                    if (remainder <= 0) {
+                        return true;
                     }
-                } else {
-                    i += 2;
                 }
             }
 
-            if (i > 0 && i < flowerbed.length - 1) {
-                if (flowerbed[i] == 0) {
-                    if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0) {
-                        flowerbed[i] = 1;
-                        remainder -= 1;
-                        i += 2;
-                    } else if (flowerbed[i - 1] == 1) {
-                        if (flowerbed[i + 1] == 0) {
-                            i += 1;
-                        } else {
-                            i += 3;
-                        }
-                    } else if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 1) {
-                        i += 3;
-                    }
-                } else {
-                    i += 2;
-                }
-            }
-
-            if (i == flowerbed.length - 1) {
-                if (flowerbed[i] == 0 && flowerbed[i - 1] == 0) {
-                    flowerbed[i] = 1;
-                    remainder -= 1;
-                } else {
-                    i++;
-                }
-            }
         }
 
-        System.out.println("remainder: " + remainder);
-        System.out.println(Arrays.toString(flowerbed));
         return remainder <= 0;
     }
 }
