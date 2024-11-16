854665c6efa3cf77ed77ae57cf7c2319629e09c8
Muhammad Helmi Hibatullah <helmihibatullah52@gmail.com>
2024-11-17 03:36:08 +0700

    Time: 2 ms (83.99%), Space: 45.8 MB (67.74%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,11 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if (nums.length > 1) {
-            int j = 0;
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == 0) {
-                    while (j < nums.length && nums[j] == 0) {
-                        j++;
-                    }
-
-                    if (j < nums.length) {
-                        int temp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = temp;
-                    }
-                }
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
                 j++;
             }
         }
