29ee7160f1af1b2852581a88978e623953dbd714
Muhammad Helmi Hibatullah <helmihibatullah52@gmail.com>
2024-11-19 18:15:49 +0700

    Time: 7 ms (97.9%), Space: 45.3 MB (10.77%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,25 +1,26 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        Map<String, Boolean> vowels = new HashMap<String, Boolean>();
-        vowels.put("a", true);
-        vowels.put("i", true);
-        vowels.put("u", true);
-        vowels.put("e", true);
-        vowels.put("o", true);
+        boolean[] vowels = new boolean['z' + 1];
+        vowels['a'] = true;
+        vowels['i'] = true;
+        vowels['u'] = true;
+        vowels['e'] = true;
+        vowels['o'] = true;
 
+        char[] sc = s.toCharArray();
         int res = 0;
         for (int i = 0; i < k; i++) {
-            if (vowels.containsKey(String.valueOf(s.charAt(i)))) {
+            if (vowels[sc[i]]) {
                 res += 1;
             }
         }
 
         int temp = res;
         for (int i = k; i < s.length(); i++) {
-            if (vowels.containsKey(String.valueOf(s.charAt(i)))) {
+            if (vowels[sc[i]]) {
                 temp += 1;
             }
-            if (vowels.containsKey(String.valueOf(s.charAt(i - k)))) {
+            if (vowels[sc[i - k]]) {
                 temp -= 1;
             }
             res = Math.max(temp, res);
