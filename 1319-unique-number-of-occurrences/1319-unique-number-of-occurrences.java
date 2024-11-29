bfbb1f17f0b114ee70aae723f19cf0bbe2dc8596
Muhammad Helmi Hibatullah <helmihibatullah52@gmail.com>
2024-11-29 09:59:48 -0800

    Time: 2 ms (98.98%), Space: 41.7 MB (72.61%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,18 +1,11 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         Map<Integer, Integer> temp1 = new HashMap<Integer, Integer>();
-        int uniqueCount = 0;
-
-        for (int nums: arr) {
-            if (temp1.containsKey(nums)) {
-                temp1.put(nums, temp1.get(nums) + 1);
-            } else {
-                temp1.put(nums, 1);
-                uniqueCount++;
-            }
+        for (int num: arr) {
+            temp1.put(num, temp1.getOrDefault(num, 0) + 1);
         }
 
         Set<Integer> values = new HashSet<Integer>(temp1.values());
-        return values.size() == uniqueCount;
+        return values.size() == temp1.size();
     }
 }
