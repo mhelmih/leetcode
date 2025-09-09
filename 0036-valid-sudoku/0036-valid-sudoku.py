042102651a0fd270a70ff9041b71cef256d477ee
Muhammad Helmi Hibatullah <helmihibatullah52@gmail.com>
2025-09-10 00:45:31 +0700

    Time: 3 ms (81.1%), Space: 18 MB (24.98%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,18 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row = [[0 for _ in range(10)] for _ in range(10)]
-        col = [[0 for _ in range(10)] for _ in range(10)]
-        box = [[0 for _ in range(10)] for _ in range(10)]
+        row = [[0 for _ in range(9)] for _ in range(9)]
+        col = [[0 for _ in range(9)] for _ in range(9)]
+        box = [[0 for _ in range(9)] for _ in range(9)]
 
         for i in range(9):
             for j in range(9):
-                if board[i][j].isdigit():
-                    num = int(board[i][j])
-                    if row[i][num] or col[j][num] or box[(i//3) * 3 + (j//3)][num]:
+                if board[i][j] != ".":
+                    num = ord(board[i][j]) - ord('1')
+                    boxIdx = (i//3) * 3 + (j//3)
+                    if row[i][num] or col[j][num] or box[boxIdx][num]:
                         return False
                     else:
                         row[i][num] = 1
                         col[j][num] = 1
-                        box[(i//3) * 3 + (j//3)][num] = 1
+                        box[boxIdx][num] = 1
 
         return True
