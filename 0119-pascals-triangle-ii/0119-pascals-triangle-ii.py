0b71a02f34694b1ade5121a882ffb51796132368
Muhammad Helmi Hibatullah <helmihibatullah52@gmail.com>
2025-10-06 22:42:12 +0700

    Time: 3 ms (9.68%), Space: 17.7 MB (81.93%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,16 +1,12 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        temp = [[1 for _ in range(rowIndex + 1)] for _ in range(rowIndex + 1)]
-        for i in range(1, rowIndex + 1):
-            for j in range(1, rowIndex + 1):
-                temp[i][j] = temp[i-1][j] + temp[i][j-1]
-        
-        ans = []
-        i = 0
-        j = rowIndex
-        while j >= 0:
-            ans.append(temp[i][j])
-            i += 1
-            j -= 1
+        if rowIndex == 0:
+            return [1]
+            
+        ans = [1 for _ in range(rowIndex + 1)]
+        previous_row = self.getRow(rowIndex - 1)
+
+        for i in range(1, len(ans) - 1):
+            ans[i] = previous_row[i] + previous_row[i - 1]
         
         return ans
